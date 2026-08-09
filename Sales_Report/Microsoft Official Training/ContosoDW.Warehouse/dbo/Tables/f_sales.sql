36c6c3be82166bc947a61b9a122d5ae67d3af623
Shoeb-BI <shoeb1359@yahoo.com>
2026-08-10 00:06:19 +0530

    Committing 6 items from workspace d1847af3-63f2-4cdb-ac61-879503d263b2

--- Sales_Report/Microsoft Official Training/ContosoDW.Warehouse/dbo/Tables/f_sales.sql
+++ Sales_Report/Microsoft Official Training/ContosoDW.Warehouse/dbo/Tables/f_sales.sql
@@ -12,4 +12,10 @@ CREATE TABLE [dbo].[f_sales] (
 
 
 GO
+ALTER TABLE [dbo].[f_sales] ADD CONSTRAINT FK_Sales_Customer FOREIGN KEY ([Customerkey]) REFERENCES [dbo].[d_Customer]([CustomerKey]);
+GO
 ALTER TABLE [dbo].[f_sales] ADD CONSTRAINT FK_Sales_Date FOREIGN KEY ([Datekey]) REFERENCES [dbo].[d_Date]([Datekey]);
+GO
+ALTER TABLE [dbo].[f_sales] ADD CONSTRAINT FK_Sales_Product FOREIGN KEY ([Productkey]) REFERENCES [dbo].[d_Product]([ProductKey]);
+GO
+ALTER TABLE [dbo].[f_sales] ADD CONSTRAINT FK_Sales_Store FOREIGN KEY ([Storekey]) REFERENCES [dbo].[d_Store]([StoreKey]);
